d6030a913694312db075dc025f86ff941c6758e0
sunjiajia3 <82640932+sunjiajia3@users.noreply.github.com>
2021-04-19 10:08:54 +0800

    Update Ssmdemo001Application.java

--- src/main/java/com/yidu/ssmdemo001/Ssmdemo001Application.java
+++ src/main/java/com/yidu/ssmdemo001/Ssmdemo001Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Ssmdemo001Application {
 
     public static void main(String[] args) {
+         System.out.println("李思是猪！");
         SpringApplication.run(Ssmdemo001Application.class, args);
     }
 
